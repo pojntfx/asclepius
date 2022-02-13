0de11a9f74068833e05055e9dd9ee7f6cf092f5e
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-02-13 23:50:56 +0100

    feat: Start implementation of command-response loop

---
+++ cmd/asclepius-token.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { MatrixAuth } from "matrix-bot-sdk";
+
+const homeserver = process.env.HOMESERVER;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+if (!homeserver || !username || !password) {
+  console.log(
+    "Please set the HOMESERVER, USERNAME and PASSWORD env variables to continue"
+  );
+
+  process.exit(1);
+}
+
+const auth = new MatrixAuth(homeserver);
+const client = await auth.passwordLogin(username, password);
+
+console.log(client.accessToken);
