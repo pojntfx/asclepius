4c33c781bca78cd2d2068ba91e198c001dc47d90
Felix Pojtinger <felix@pojtinger.com>
2022-02-20 21:48:08 +0100

    feat: Add basic in-code docs

--- cmd/asclepius-token.js
+++ cmd/asclepius-token.js
@@ -2,10 +2,10 @@
 
 import { MatrixAuth } from "matrix-bot-sdk";
 
+// Get configuration from environment
 const homeserver = process.env.HOMESERVER;
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
-
 if (!homeserver || !username || !password) {
   console.error(
     "Please set the HOMESERVER, USERNAME and PASSWORD env variables to continue"
@@ -14,7 +14,9 @@ if (!homeserver || !username || !password) {
   process.exit(1);
 }
 
+// Connect to Matrix
 const auth = new MatrixAuth(homeserver);
 const client = await auth.passwordLogin(username, password);
 
+// Print the access token
 console.log(client.accessToken);
